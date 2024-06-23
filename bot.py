d91c8cea3d619cc5e685e60de9386acf9f00496c
sattorov <sattorov.ahu@gmail.com>
2024-06-23 12:01:07 +0500

    initial commit

--- bot.py
+++ bot.py
@@ -6,13 +6,13 @@ client = TelegramClient(session='client', api_id=config.API_ID, api_hash=config.
 
 
 @bot.on(events.NewMessage(pattern='/start'))
-async def start(event):
+async def handle_start_command(event):
     await event.respond('Salom! Bu Telethon yordamida yaratilgan Telegram botidir.')
     raise events.StopPropagation
 
 
 @bot.on(events.NewMessage(pattern='/help'))
-async def help_(event):
+async def handle_help_command(event):
     await event.respond(
         'Quyidagi komandalar mavjud:\n'
         '/start - Botni boshlash\n'
@@ -22,14 +22,17 @@ async def help_(event):
 
 
 @bot.on(events.NewMessage(pattern='#post'))
-async def handle_start_command(event):
+async def handle_post_command(event):
     group_ids = [-1002224182641]
     await client.connect()
-
+    text_l = str(event.message.message).split('#post')[1:]
+    text = "".join(text_l)
     for i in group_ids:
         await client.send_message(
-            i, str(event.message.message).split()[1]
+            i, text
         )
+
+    await event.respond("Habar yuborildi!")
     raise events.StopPropagation
 
 
