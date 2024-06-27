27223b257d6f377b1764b56779d37ba33fa1fca6
sattorov <sattorov.ahu@gmail.com>
2024-06-27 15:41:59 +0500

    bot.

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 from telethon import TelegramClient, events
 import config
+import time
 
 bot = TelegramClient(session='bot', api_id=config.API_ID, api_hash=config.API_HASH).start(bot_token=config.BOT_TOKEN)
 client = TelegramClient(session='client', api_id=config.API_ID, api_hash=config.API_HASH)
+
+GROUP_IDS = [-1002224182641, -1002224182641, -1002224182641, -1002224182641]
 
 
 @bot.on(events.NewMessage(pattern='/start'))
@@ -23,16 +26,17 @@ async def handle_help_command(event):
 
 @bot.on(events.NewMessage(pattern='#post'))
 async def handle_post_command(event):
-    group_ids = [-1002224182641]
-    await client.connect()
-    text_l = str(event.message.message).split('#post')[1:]
-    text = "".join(text_l)
-    for i in group_ids:
-        await client.send_message(
-            i, text
-        )
+    if int(event.peer_id.user_id) in list(map(int, config.ADMINS)):
+        await client.connect()
+        text_l = str(event.message.message).split('#post')[1:]
+        text = "".join(text_l)
+        for g_id in GROUP_IDS:
+            await client.send_message(
+                g_id, text
+            )
+            time.sleep(2)
 
-    await event.respond("Habar yuborildi!")
+        await event.respond("Habar yuborildi!")
     raise events.StopPropagation
 
 
